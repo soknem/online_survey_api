4a6fde1218320a1f58ae408879ca88c389c28d43
soknem <sokname56@gmail.com>
2025-12-18 00:25:08 +0700

    Auto commit

--- init-scripts/01_setup_user.sql
+++ init-scripts/01_setup_user.sql
@@ -1,9 +1,6 @@
--- 1. Switch to the Pluggable Database (PDB)
--- Your logs confirmed the name is FREEPDB1
+
 ALTER SESSION SET CONTAINER = FREEPDB1;
 
--- 2. Create the 'sa' user
--- We use a basic block to handle the case where the user might already exist
 BEGIN
 EXECUTE IMMEDIATE 'DROP USER sa CASCADE';
 EXCEPTION
@@ -16,12 +13,10 @@ END;
 
 CREATE USER sa IDENTIFIED BY sa;
 
--- 3. Grant necessary permissions for a Spring Boot application
 GRANT CONNECT, RESOURCE, DBA TO sa;
 GRANT CREATE SESSION TO sa;
 GRANT UNLIMITED TABLESPACE TO sa;
 
--- 4. Final verification settings
 ALTER USER sa ACCOUNT UNLOCK;
 
 COMMIT;
